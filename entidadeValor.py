f9bff2c1c27a236ec5652357f20a9a3afd30b414
vpcsilva <vpcx5@yahoo.com.br>
2016-10-18 02:03:52 -0200

    Refactoring: Movendo elementos cardinalidade Many para outro método

--- entidadeValor.py
+++ entidadeValor.py
@@ -8,6 +8,12 @@ def elementosCardinalidadeOne(atributo, fatos):
         lista_retorno.append( list(g)[-1] )
     return lista_retorno
 
+def elementosCardinalidadeMany( atributo, fatos):
+    lista_many = list( filter( lambda x: x[1] == atributo[0], fatos ) )
+    remocoes = list( filter( lambda x: not x[-1], lista_many ) )
+    for remove in remocoes:
+        lista_many = list( filter( lambda x: x[2] != remove[2], lista_many ) )
+    return lista_many
 
 def entidadesAtivas(fatos, schema):
     lista_retorno = []
@@ -15,10 +21,6 @@ def entidadesAtivas(fatos, schema):
         if atributo[2] == 'one':
             lista_retorno.extend( elementosCardinalidadeOne(atributo, fatos) )
         else:
-            lista_many = list( filter( lambda x: x[1] == atributo[0], fatos ) )
-            remocoes = list( filter( lambda x: not x[-1], lista_many ) )
-            for remove in remocoes:
-                lista_many = list( filter( lambda x: x[2] != remove[2], lista_many ) )
-            lista_retorno.extend( lista_many )
+            lista_retorno.extend( elementosCardinalidadeMany(atributo, fatos) )
 
     return lista_retorno
