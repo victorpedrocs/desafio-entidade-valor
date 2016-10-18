b9f16fd4d61cc96e294a08973ba8417a60c4bd37
vpcsilva <vpcx5@yahoo.com.br>
2016-10-18 01:35:02 -0200

    Refactor: Método para retornar list com elementos de cardinalidade 1

--- entidadeValor.py
+++ entidadeValor.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from itertools import groupby
 
+def elementosCardinalidadeOne(atributo, fatos):
+    lista_retorno = []
+    atributos_one = list( filter( lambda x: x[1] == atributo[0], fatos ) )
+    for k, g in groupby( atributos_one, lambda x: x[1] ):
+        lista_retorno.append( list(g)[-1] )
+    return lista_retorno
+
+
 def entidadesAtivas(fatos, schema):
     lista_retorno = []
     for atributo in schema:
         if atributo[2] == 'one':
-            atributos_one = list( filter( lambda x: x[1] == atributo[0], fatos ) )
-            for k, g in groupby( atributos_one, lambda x: x[1] ):
-                lista_retorno.append( list(g)[-1] )
+            lista_retorno.extend( elementosCardinalidadeOne(atributo, fatos) )
         else:
             lista_retorno.extend( list( filter( lambda x: x[1] == atributo[0], fatos ) ) )
 
